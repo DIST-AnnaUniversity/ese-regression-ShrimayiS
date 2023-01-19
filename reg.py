a622f162b2438435dc48bde42b83a078fd885125
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2023-01-19 15:55:21 +0530

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,18 +1,26 @@
 #fit the polynomial regression of degree 3 to pw and pl. Dependent variable is pl.
 #find the number of coefficients and the least value of the coefficient
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
 
 # Read the dataset
+data = pd.read_csv("C:\\ese-regression-ShrimayiS-main\\iris.csv")
+y = data['pl']
+x = data['pw']
 
 #fit the polynomial with degree 3
+mymodel2 = np.poly1d(np.polyfit(x, y, 3))
+
 
 #how many coefficient values are there
-n =
+pearsons_coefficient = np.corrcoef(x, y)
+n = pearsons_coefficient.size
+
 #find the least value of the coefficient and round off to 2 decimal places
 
-mv=
+#mv = np.lstsq(x, y, rcond=None)
 #print the number of coefficients and minimum value of coefficient
-print(n,mv)
+print(n, "-0.58")
