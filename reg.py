8b418a6dddfd9f6767c35c5f88f8226647f27552
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2023-01-19 15:56:32 +0530

    Update reg.py

--- reg.py
+++ reg.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 # Read the dataset
-data = pd.read_csv("C:\\ese-regression-ShrimayiS-main\\iris.csv")
+data = pd.read_csv("iris.csv")
 y = data['pl']
 x = data['pw']
 
