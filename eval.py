51ad608801d8653ca472fad641cdcdb192fab816
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2023-01-19 15:54:15 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -2,18 +2,27 @@
 #If same, print "same" (round off the r2 score to 2 decimal places)
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
 import numpy as np
 import pandas as pd
 
 # Read the dataset
-
-
+data = pd.read_csv("C:\\ese-regression-ShrimayiS-main\\iris.csv")
+y = data['pl']
+x = data['pw']
 
 #fit the polynomial with degree 2 and 3
+mymodel = np.poly1d(np.polyfit(x, y, 2))
+mymodel2 = np.poly1d(np.polyfit(x, y, 3))
+
+
 
 #evaluate both polynomials with r2 score and round off to 2 decimals
-n1 =
-n2 =
 
+n1 =round((r2_score(y, mymodel(x))),2)
+#print(n1)
+n2 =round((r2_score(y, mymodel2(x))),2)
+#print(n2)
 if n1 == n2:
     print("same")
