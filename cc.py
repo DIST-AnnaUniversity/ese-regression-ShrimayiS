9f3cca63660cb6cc5c8c759a6fd13864918df039
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2023-01-19 15:53:47 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 
 # Read the dataset
-
+data = pd.read_csv("C:\\ese-regression-ShrimayiS-main\\iris.csv")
 
 # Take dependent variable as 'pl' and independent variable as 'pw'
-
-
+y = data['pl']
+x = data['pw']
+pearsons_coefficient = np.corrcoef(x, y)
 
 
 
 # Print the correlation coefficient and round off to 2 decimal places
+print(np.round(pearsons_coefficient,decimals = 2, out=None))
